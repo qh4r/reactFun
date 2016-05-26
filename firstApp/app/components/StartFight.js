08eb3c29b3225c17e25e97ddc823acc56b715065
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-26 14:56:21 +0200

    Reworked rendering funcion int component

--- firstApp/app/components/StartFight.js
+++ firstApp/app/components/StartFight.js
@@ -1,15 +1,34 @@
 var React = require('react');
 
-function show (obj){
+//function show (obj){
+//    return (
+//        <pre>{JSON.stringify(obj, null, '\n')}</pre>
+//    )
+//}
+//
+//var StartFight = function(props){
+//    return (
+//        <div>{props.isLoading?<p> LOADING </p>: <p> {show(props)} </p>}</div>
+//    )
+//};
+
+//ODPOWIEDNIK FUNKCYJNY u GORY
+//NA DOLE KOMPONENT
+//NAZWY KOMPONENTOW MUSZA ZACZYNAC SIE OD DUZEJ LITERY!!!!!
+
+var Show = function (obj) {
     return (
-        <pre>{JSON.stringify(obj, null, '\n')}</pre>
+            <pre>{JSON.stringify(obj, null, '\n')}</pre>
     )
 }
 
-var StartFight = function(props){
+var StartFight = function (props) {
     return (
-        <div>{props.isLoading?<p> LOADING </p>: <p> {show(props)} </p>}</div>
+        <div>{props.isLoading ? <p> LOADING </p> :
+            <Show data={props}></Show>
+        }</div>
     )
 };
+
 
 module.exports = StartFight;
