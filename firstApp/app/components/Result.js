93b09b239debba854605ed38538161a60a8d33a2
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-28 01:58:32 +0200

    Added result screen

--- firstApp/app/components/Result.js
+++ firstApp/app/components/Result.js
@@ -1,16 +1,49 @@
 var React = require('react');
+require('../styles/bootstrap.css');
+require('../styles/index.css');
+var PlayerDetails = require('../components/PlayerDetails'),
+    PlayerDetailsWrapper = require('../components/PlayerDetailsWrapper'),
+    Link = require('react-router').Link;
 
-var Show = function(props){
+var ResultBody  = function (props) {
     return (
-        <pre>{JSON.stringify(props, 2, '\n')}</pre>
-    )
-}
-
-var Result = function(props){
-    return (
-        <div>wynik
-        <Show scores={props.scores} playersInfo={props.playersInfo} />
+        <div className="jumbotron col-sm-12 text-center transparentBg">
+            <h1>{props.header}</h1>
+            {props.children}
+            <div className="col-sm-12 space">
+                <Link to='/firstPlayer'>
+                    <button className='btn btn-lg btn-danger'>Od początku</button>
+                </Link>
+            </div>
         </div>
+    )
+};
+
+var Result = function (props) {
+    if(props.isLoading){
+        return (
+            <p>Wczytuję...</p>
+        )
+    }
+
+    if(props.scores[0] === props.scores[1]){
+        return (
+            <ResultBody header="Remis">
+            </ResultBody>
+        )
+    }
+    var winnerIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+    return (
+        <ResultBody header="Wyniki">
+            <div className="col-sm-8 col-sm-offset-2">
+                <PlayerDetailsWrapper displayName="Zwycięzca">
+                    <PlayerDetails score={props.scores[winnerIndex] || 0} user={props.playersInfo[winnerIndex]}/>
+                </PlayerDetailsWrapper>
+                <PlayerDetailsWrapper displayName="Przegryw">
+                    <PlayerDetails score={props.scores[1-winnerIndex] || 0} user={props.playersInfo[1-winnerIndex]}/>
+                </PlayerDetailsWrapper>
+            </div>
+        </ResultBody>
     )
 };
 
