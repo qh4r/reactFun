d99cc7a4be197f7bad951e93db409a699abfcc85
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-29 22:41:09 +0200

    Added transitions and updated node

--- firstApp/app/components/Main.js
+++ firstApp/app/components/Main.js
@@ -1,10 +1,18 @@
+require('../styles/index.css');
+
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 var React = require('react');
 
 var Main = React.createClass({
-    render: function(){
+    render: function () {
         return (
             <div className="main-container">
-                {this.props.children}
+                <ReactCSSTransitionGroup
+                    transitionName='appear'
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}>
+                    {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
