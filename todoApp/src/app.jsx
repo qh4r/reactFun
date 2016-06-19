28533ab7d5f9b97adb5b37224f7e2fc9d9cf4a92
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-06-19 23:33:19 +0200

    Set up connection with database

--- todoApp/src/app.jsx
+++ todoApp/src/app.jsx
@@ -1,13 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase')
+var baseUrl = 'https://fluxtest-49f41.firebaseio.com';
+//var baseUrl = 'https://blistering-torch-4253.firebaseio.com';
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+var App = React.createClass({
+    mixins: [ReactFire],
+    componentWillMount: function () {
+        //Pochodzi z reactFire
+        var fb = new Firebase(baseUrl + '/items/');
+        this.bindAsObject(fb, 'items');
+        console.log(fb);
+        //this.state.items zawiera dane
+    },
+    render: function () {
+        console.log('state ', this.state);
+        return <div>
+            <h1>
+                Hello!
+            </h1>
+        </div>
+    }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
