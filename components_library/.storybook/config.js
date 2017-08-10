48f848bfd7e5da7cc306f29972d6c3e445c0a6fc
Rafał Kucharski <qhr666@gmail.com>
2017-08-10 22:03:05 +0200

    Played around with storybook addons

--- components_library/.storybook/config.js
+++ components_library/.storybook/config.js
@@ -1,7 +1,14 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 function loadStories() {
   require('../stories/index.js');
 }
+
+setOptions({
+  name: 'My book',
+  downPanelInRight: true,
+  sidebarAnimations: true,
+});
 
 configure(loadStories, module);
--- components_library/stories/hello-story.js
+++ components_library/stories/hello-story.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Hello from '../src/components/Hello/Hello';
+import { withNotes } from '@storybook/addon-notes';
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 
 // using '/' you cen group elements
 storiesOf('App/Hello', module)
+  // knobs allow you to  edit values in storybook
+  .addDecorator(withKnobs)
   .add('Billy', () => (
-    <Hello msg="Billy" />
+    <Hello msg={text('MSG', 'Billy')} />
   ))
-  .add('None', () => (
+  // INFO is much more practical then notes - displays props and stuff
+  .add('None', withInfo('component using default values')(withNotes('this component uses default value')(() => (
     <Hello />
-  ));
+  ))));
 //actions get registered with given anme and args
 storiesOf('App/Buttons/plain', module)
-  .add('click', () => (
+  .add('click', withNotes('this is just a sample button')(() => (
     <button onClick={action('clicked!')}>click</button>
-  ))
+  )))
