d856e5fc26c7eb329fab9e5793913853dc41e102
Rafał Kucharski <qhr666@gmail.com>
2017-12-17 15:10:35 +0100

    Played around with hoc patters

--- component_patterns/src/App.js
+++ component_patterns/src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 import Toggle from './Toogle/toogle.componen';
 
+const Button = Toggle.withToggle(function (props) {
+  return <button onClick={props.toggle}>CHNAGE!</button>
+});
+
+
+class TestStatics extends React.Component {
+  // gets copie thx to hoistNonReactStatics
+  static OptionalMessage = Toggle.withToggle(
+    ({ on }) =>
+      on
+        ? 'Warning: The button is toggled on'
+        : null);
+
+  render() {
+    const { on, toggle } = this.props;
+    return (
+      <button onClick={toggle}>
+        {on ? 'on' : 'off'}
+      </button>
+    )
+  }
+}
+
+const TestWrapper = Toggle.withToggle(TestStatics);
+
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{margin: "40px"}}>
-        <Toggle onToggle={(status) => console.log("new status is: %s!", status)} />
+      <div className="App" style={{ margin: "40px" }}>
+        <Toggle onToggle={(status) => console.log("new status is: %s!", status)}>
+          <Toggle.On>IS ON</Toggle.On>
+          <Toggle.Button />
+          <div>
+            <Toggle.Off>IS OFF</Toggle.Off>
+          </div>
+          <Button />
+          <br />
+          <TestWrapper />
+          <TestWrapper.OptionalMessage />
+        </Toggle>
       </div>
     );
   }
--- component_patterns/src/Toogle/toogle.componen.js
+++ component_patterns/src/Toogle/toogle.componen.js
@@ -1,11 +1,70 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Switch from '../Switch/switch.component';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+const TOGGLE_CONTEXT = '__toggle__';
+
+// Compound components
+
+
+function withToggle(Component) {
+  const Wrapper = class extends React.Component {
+    static contextTypes = {
+      [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+    };
+
+    render() {
+      return (<Component {...this.props} {...this.context[TOGGLE_CONTEXT]} />);
+    }
+  };
+  Wrapper.displayName = 'withToggle';
+
+  // copies non react statics from Component to Wrapper (and returns wrapper);
+  return hoistNonReactStatics(Wrapper, Component);
+}
+
+function ToggleOn({ children }, context) {
+  const { on } = context[TOGGLE_CONTEXT];
+  return on ? children : null
+}
+
+ToggleOn.contextTypes = {
+  [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+};
+
+function ToggleButton(props, context) {
+  const { on, toggle } = context[TOGGLE_CONTEXT];
+  return (
+    <Switch on={on} onClick={toggle} {...props} />
+  )
+}
+
+ToggleButton.contextTypes = {
+  [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+};
+
+// can use with hoc
+
+const ToggleOff = withToggle(({on, children}) => on ? null : children);
+
+ToggleOff.contextTypes = {
+  [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+};
+
 
 class Toggle extends React.Component {
   static defaultProps = {
     onToggle: () => {
     },
   };
+  static On = ToggleOn;
+  static Off = ToggleOff;
+  static Button = ToggleButton;
+  static withToggle = withToggle;
+  static childContextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  }
 
   state = { on: false };
 
@@ -17,12 +76,33 @@ class Toggle extends React.Component {
       },
     );
 
+  getChildContext() {
+    return {
+      [TOGGLE_CONTEXT]: {
+        on: this.state.on,
+        toggle: this.toggle,
+      },
+    }
+  }
+
   render() {
-    const { on } = this.state;
+    // USAGE W/O context and control
+
+    // its best practice to clone elements instead mutating them when mapping children
+
+    // const children = React.Children.map(
+    //   this.props.children,
+    //   child => React.cloneElement(child, {
+    //     on: this.state.on,
+    //     toggle: this.toggle,
+    //   }),
+    // );
     return (
-      <Switch on={on} onClick={this.toggle} />
+      <div>{this.props.children}</div>
     )
   }
 }
+
+// <Switch on={on} onClick={this.toggle} />
 
 export default Toggle;
